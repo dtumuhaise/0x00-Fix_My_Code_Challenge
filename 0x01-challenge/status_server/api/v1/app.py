305ec46204c681d9fbae9c3c0f58f0a031c2d076
Davis Tumuhaise <tumuhaise@gmail.com>
2023-02-07 12:55:39 +0300

    fix

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -8,6 +8,11 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.route('/status')
+def status():
+    """ json status endpoint """
+    return jsonify({"status": "OK"})
+
 
 @app.errorhandler(404)
 def not_found(error):
